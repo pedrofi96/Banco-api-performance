db7a35c45a08434f1b87f37b0c583e1a7ba96bb1
Pedro Filipe  Tavares Baptista <109918486+pedrofi96@users.noreply.github.com>
2025-11-05 17:08:20 -0300

    Adicionando VU(Virtual Users) e duração do teste para ser executado

--- test/login.test.js
+++ test/login.test.js
@@ -1,14 +1,15 @@
 // Import the http module to make HTTP requests. From this point, you can use `http` methods to make HTTP requests.
-import http from 'k6/http'
+import http from "k6/http";
 // Import the sleep function to introduce delays. From this point, you can use the `sleep` function to introduce delays in your test script.
-import { sleep, check } from 'k6'
+import { sleep, check } from "k6";
 
 export const options = {
-  // Define the number of iterations for the test
-  iterations: 10,
+  
+  vus: 10, // usuarios vituais
+  duration: "30s", // duraçãod a interação
   thresholds: {
-    http_req_duration: ['p(90)<10', 'max<10'], //percentil 90 abaixo de 10 ms
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ["p(90)<3000", "max<5000"], //percentil 90 abaixo de 10 ms
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
   },
 };
 
@@ -16,26 +17,26 @@ export const options = {
 export default function () {
   //Teste!!!
 
- const url = 'http://localhost:3000/login';
+  const url = "http://localhost:3000/login";
 
   const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-  })
+    username: "julio.lima",
+    senha: "123456",
+  });
 
   const params = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }
+  };
 
   const res = http.post(url, payload, params);
 
-  check(res,{
-    'Validar o Status 200': (r)=> r.status === 200,
-    'Validar que o token é uma string': (r) => typeof(r.json().token) == 'string'
-  })
+  check(res, {
+    "Validar o Status 200": (r) => r.status === 200,
+    "Validar que o token é uma string": (r) =>
+      typeof r.json().token == "string",
+  });
 
-  sleep(1)
-  
+  sleep(1);
 }
